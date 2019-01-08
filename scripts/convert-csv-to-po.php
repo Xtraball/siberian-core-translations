0f586b01af482531103f1dcb80cbc00797c2e9c0
Alex-Yves CREPIN <a.crepin@xtraball.com>
2019-01-08 16:36:03 +0100

    feat: improves base files

--- scripts/convert-csv-to-po.php
+++ scripts/convert-csv-to-po.php
@@ -2,59 +2,42 @@
 
 chdir(__DIR__ . "/..");
 
-$codes = [
-    "base",
-    "ca",
-    "cs",
-    "de",
-    "el",
-    "es",
-    "fr",
-    "it",
-    "nl",
-    "pt_BR",
-    "ru",
-    "tr",
-    "zh",
-];
-
-foreach ($codes as $code) {
-    $files = new DirectoryIterator("./{$code}");
-    foreach ($files as $file) {
-        if (!$file->isDot() && $file->getExtension() === "csv") {
-            echo "File: " . $file->getFilename() . PHP_EOL;
-            $newValues = [];
-            $lineNumber = 1;
-            $resource = fopen($file->getPathname(), "r");
-            while ($content = fgetcsv($resource, 1024, ";", '"')) {
-                $key = str_replace('\"', '"', $content[0]);
-                if (isset($content[1])) {
-                    $value = str_replace('\"', '"', $content[1]);
-                } else {
-                    $value = $key;
-                }
-
-                $newValues[] = [
-                    "ref" => "{$code}/{$file->getFilename()}:{$lineNumber}",
-                    "msgid" => $key,
-                    "msgstr" => $value,
-                ];
-
-                $lineNumber++;
+$files = new DirectoryIterator("./.source");
+foreach ($files as $file) {
+    if (!$file->isDot() && $file->getExtension() === "csv") {
+        echo "File: " . $file->getFilename() . PHP_EOL;
+        $newValues = [];
+        $lineNumber = 1;
+        $resource = fopen($file->getPathname(), "r");
+        while ($content = fgetcsv($resource, 1024, ";", '"')) {
+            $key = str_replace('\"', '"', $content[0]);
+            if (isset($content[1])) {
+                $value = str_replace('\"', '"', $content[1]);
+            } else {
+                $value = $key;
             }
 
-            $newContent = "";
-            foreach ($newValues as $newValue) {
-                $newContent .= "#: {$newValue["ref"]} 
-msgid \"{$newValue["msgid"]}\"
+            $newValues[] = [
+                "msgid" => $key,
+                "msgstr" => $value,
+            ];
+
+            $lineNumber++;
+        }
+
+        $newContent = "";
+        foreach ($newValues as $newValue) {
+            $newContent .= "msgid \"{$newValue["msgid"]}\"
 msgstr \"{$newValue["msgstr"]}\"
 
 ";
-            }
-
-            file_put_contents(str_replace(".csv", ".po", $file->getPathname()), $newContent);
-            echo "Done" . PHP_EOL;
         }
 
+        $newPath = str_replace(".csv", ".po", $file->getPathname());
+        $newPath = str_replace(".source", "base", $newPath);
+
+        file_put_contents($newPath, $newContent);
+        echo "Done" . PHP_EOL;
     }
+
 }
