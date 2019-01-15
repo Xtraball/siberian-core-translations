6dedc679f6b7345cd51025ac24d261afda12d379
Alex-Yves CREPIN <a.crepin@xtraball.com>
2019-01-15 14:41:30 +0100

    feat: updated all .po files

---
+++ scripts/pull-latest.php
@@ -0,0 +1,93 @@
+<?php
+
+// https://translate.siberiancms.com/download/siberian/default-group2/fr/?format=po
+
+$endpoint = "https://translate.siberiancms.com/download/siberian/#COMPONENT#/#LANG#/?format=po";
+
+chdir(__DIR__ . "/..");
+
+$locales = [
+    "ca" => "Catalan",
+    "cs" => "Czech",
+    "de" => "German",
+    "el" => "Greek",
+    "es" => "Spanish",
+    "fr" => "French",
+    "it" => "Italian",
+    "nl" => "Dutch",
+    "pt_BR" => "PortugueseBrazil",
+    "ru" => "Russian",
+    "tr" => "Turkish",
+    "zh_Hans" => "ChineseSimplified",
+];
+
+$components = [
+    "acl",
+    "admin",
+    "api",
+    "application",
+    "backoffice",
+    "booking",
+    "catalog",
+    "cms",
+    "codescan",
+    "comment",
+    "contact",
+    "customer",
+    "default-group1",
+    "default-group2",
+    "default",
+    "event",
+    "features",
+    "folder",
+    "form",
+    "installer",
+    "loyaltycard",
+    "maps",
+    "mcommerce",
+    "media",
+    "message",
+    "mobile",
+    "padlock",
+    "payment",
+    "preview",
+    "promotion",
+    "push",
+    "radio",
+    "rss",
+    "sales",
+    "social",
+    "socialgaming",
+    "sourcecode",
+    "subscription",
+    "system",
+    "tax",
+    "template",
+    "tip",
+    "topic",
+    "translation",
+    "twitter",
+    "weather",
+    "weblink",
+    "whitelabel",
+    "wordpress",
+];
+
+$total = count($locales) * count($components);
+$counter = 0;
+
+foreach ($locales as $folder => $human) {
+    echo "Current locale: {$human} ({$folder}) \n\n";
+    foreach ($components as $component) {
+        $percentage = round($counter / $total * 100, 2);
+        echo "Current component: {$component} {$percentage}% \n";
+        $url = str_replace("#COMPONENT#", $component, $endpoint);
+        $url = str_replace("#LANG#", $folder, $url);
+
+        $content = file_get_contents($url);
+
+        file_put_contents("./{$folder}/{$component}.po", $content);
+        $counter++;
+    }
+}
+
